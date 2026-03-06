3920c89d037165f7f3fd90692b388bcc9b5b4067
Lucas Marafigo <marafigo.lucas16@gmail.com>
2026-03-06 12:43:34 -0300

    Order, Instant, ISO 8601-part2

--- src/main/java/com/educandoweb/course/repositories/OrderRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.educandoweb.course.repositories;
 
-public interface OrderRepository {
+import com.educandoweb.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
